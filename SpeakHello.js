d05014ac3ecf1f3dff23ed4af5cf28c8186fc0c9
Sofia Alfaro <134465624+asofialf@users.noreply.github.com>
2023-10-01 19:18:25 -0500

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -15,7 +15,7 @@ var hi = "Hello";
 // See Lecture 52, part 2
 (function(window) {
 	var helloSpeaker = function (name) {
-		console.log(hi + " " + name);
+		console.log("Hello " + name);
 	}
 
 	window.helloSpeaker = helloSpeaker;
